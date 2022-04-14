23d8a0bc0f55e4f9658d70b876ed2875adcb0fca
Cybertron-Ant <mediainformationofficer@gmail.com>
2022-04-14 12:03:09 -0500

    serve multiple website file locations

--- main.js
+++ main.js
@@ -14,25 +14,63 @@ var fs = require('fs');
 //(res) response data + response headers
 //(res) is also a writable object
 var server = http.createServer(function(req, res){
+  
   //(url) is a property of the (req object)
   console.log("Request was made: " + req.url);
+  
   //write a response header
   //pass in parameter(200) response header status means "ok"
   //pass in an object as a parameter
-  //treat content type as a HTML file
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  //create a readstream
-//(__dirname) gets current working directory
-//be sure to specify the character encoding (utf-8) as a parameter
-//specify HTML filename that you want to serve/stream/pipe/send
-var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+  //send PLAIN text response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  //end response and send back 'text/HTML'
+  if (req.url === "/home" || req.url === "/") {
+    
+    //status header is 200(okay)
+    //send HTML response header
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    //read the website file and pipe it to the response objec
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+
+  } else if (req.url === "/contact") {
+    
+    //status header is 200(okay)
+    //send HTML response header
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    //read the contact.html file and pipe it to the response object
+    fs.createReadStream(__dirname + "/contact.html").pipe(res);
+
+//api & ant is a custom URL generated on the fly
+  } else if (req.url === "/api/ant") {
+  
+    var team = [
+      {name: "Kakashi Hatake", age: 20},
+      {name: "Obito Uchiha", age: 21}
+    ];
+                
+    //send JSON response header
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    
+    //transform JSON array into a string
+    res.end(JSON.stringify(team));
+
+  } else {
+    
+    //send HTML response(404) header
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    
+    //serve the 404 error page
+    fs.createReadStream(__dirname + "/404.html").pipe(res);
+  }
+
+  //end response and send back 'text/plain'
   //the (res) is an object that I can write/pipe to
-  myReadStream.pipe(res);
+  //res.end('I am the King of Games!');
 
 });
 
 
 server.listen(3000, '127.0.0.1');
-console.log('listening to port for you now');
+console.log('listening to port for you now master Kaiba');
