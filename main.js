1a94783878872eda365a966b5e42ec80ad8c0f97
Cybertron-Ant <mediainformationofficer@gmail.com>
2022-04-14 08:54:31 -0500

    require the exported function, to read the length of an array

---
+++ main.js
@@ -0,0 +1,12 @@
+//console.log('Hello Antonio Fuller!');
+
+//this file contains a variable that I exported
+//I have to assign what was "required" to a new variable
+var counter = require('./array_length');
+
+
+
+
+
+console.log(counter(["Antonio", "Behn", "Fuller"]));
+
