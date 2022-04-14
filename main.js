700c31bcaeb63b1effab36618f138b651ed28c24
Cybertron-Ant <mediainformationofficer@gmail.com>
2022-04-14 13:24:14 -0500

    SYNCHRONOUSLY read the contents of a file, then write the chunks that were read, to thr console

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 
+
+
 var fs = require('fs');
 
 
 //store what was read in variable "read"
-//the third parameter is a "callback function"
-//the function returns an error if not successful
-//this is "Asynchronous"
-fs.readFile('word.txt', 'utf-8', function(error, data){
-  console.log(data);
-
-});
+var read = fs.readFileSync('word.txt', 'utf-8');
 
 
-//can run other code while file is being read
-
-
-
+console.log(read);
 
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 
 
-ASYNCHRONOUSLY read the contents of a file, then write the chunks that were read, to thr console
+SYNCHRONOUSLY read the contents of a file, then write the chunks that were read, to thr console
