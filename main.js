b6032c43014db2d8f0af8bbb970c81f8a23643c7
Cybertron-Ant <mediainformationofficer@gmail.com>
2022-04-14 13:18:06 -0500

    ASYNCHRONOUSLY read the contents of a file, then write the chunks that were read, to thr console

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 
-
-
-
 var fs = require('fs');
 
 
-
+//store what was read in variable "read"
 //the third parameter is a "callback function"
 //the function returns an error if not successful
 //this is "Asynchronous"
-fs.readFile('word.txt', 'utf-8', function(error,data) {
+fs.readFile('word.txt', 'utf-8', function(error, data){
+  console.log(data);
 
-//pass (error) as an argument in case it fails
-//(error) is the callback function
-//if file does not exist, it will create and write to it
-  fs.writeFile('new.txt', data, (error) => {} );
 });
 
 
-//can run other code while file is being written to
+//can run other code while file is being read
+
+
+
+
